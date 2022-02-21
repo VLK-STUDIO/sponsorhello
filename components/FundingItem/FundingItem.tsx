d28f4784c4bbe481b4d72d2a54947782f5cb245e
mauroerta <mauro@vlkstudio.com>
2022-02-21 23:56:17 +0100

    fix: wrong import path

--- components/FundingItem/FundingItem.tsx
+++ components/FundingItem/FundingItem.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/Image";
+import Image from "next/image";
 import { FundingLink } from "./FundingLink";
 import { ProjectInfo } from "../../types/projects";
 import styles from "./FundingItem.module.css";
