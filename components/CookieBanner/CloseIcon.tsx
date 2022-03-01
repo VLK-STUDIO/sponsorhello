89cbf7fa40821fb305b197ee7f613dc3f379bb54
mauroerta <mauro@vlkstudio.com>
2022-03-02 00:30:42 +0100

    feat: added gtm

---
+++ components/CookieBanner/CloseIcon.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+type Props = {
+  className: string;
+  onClick: () => void;
+};
+
+const CloseIcon: React.VFC<Props> = ({ className, onClick }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="#000000"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={className}
+    onClick={onClick}
+  >
+    <circle cx="12" cy="12" r="10"></circle>
+    <line x1="15" y1="9" x2="9" y2="15"></line>
+    <line x1="9" y1="9" x2="15" y2="15"></line>
+  </svg>
+);
+export default CloseIcon;
