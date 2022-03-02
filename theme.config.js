37fe0607bf2535666407c4145f87341883247f27
mauroerta <mauro@vlkstudio.com>
2022-03-02 15:57:27 +0100

    feat: added social images

--- theme.config.js
+++ theme.config.js
@@ -23,20 +23,6 @@ module.exports = {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta
-        name="description"
-        content="Sponsorhello: sponsor the projects you love"
-      />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@VlkStudio" />
-      <meta
-        property="og:title"
-        content="Sponsorhello: fund the projects you love"
-      />
-      <meta
-        property="og:description"
-        content="Sponsorhello: fund the projects you love"
-      />
       <meta name="apple-mobile-web-app-title" content="Sponsorhello" />
       <link
         rel="icon"
@@ -44,6 +30,57 @@ module.exports = {
         media="(prefers-color-scheme: light)"
       />
       <link rel="icon" href="/faviconWhite.ico" />
+
+      <meta charset="utf-8" />
+      <title>Sponsorhello - Fund the projects you love</title>
+
+      <meta
+        name="description"
+        content="With Sponsorhello you can check how many open source projects in your package.json are searching for funding in a couple of seconds."
+      />
+      <meta name="image" content="https://sponsorhello.org/social.png" />
+
+      <meta
+        itemProp="name"
+        content="Sponsorhello - Fund the projects you love"
+      />
+      <meta
+        itemProp="description"
+        content="With Sponsorhello you can check how many open source projects in your package.json are searching for funding in a couple of seconds."
+      />
+      <meta itemProp="image" content="https://sponsorhello.org/social.png" />
+
+      <meta
+        name="twitter:description"
+        content="With Sponsorhello you can check how many open source projects in your package.json are searching for funding in a couple of seconds."
+      />
+      <meta name="twitter:card" content="summary_large" />
+      <meta
+        name="twitter:title"
+        content="Sponsorhello - Fund the projects you love"
+      />
+      <meta name="twitter:site" content="@VlkStudio" />
+      <meta name="twitter:creator" content="@VlkStudio" />
+      <meta
+        name="twitter:image:src"
+        content="https://sponsorhello.org/social.png"
+      />
+
+      <meta
+        name="og:title"
+        content="Sponsorhello - Fund the projects you love"
+      />
+      <meta
+        name="og:description"
+        content="With Sponsorhello you can check how many open source projects in your package.json are searching for funding in a couple of seconds."
+      />
+      <meta name="og:image" content="https://sponsorhello.org/social.png" />
+      <meta name="og:url" content="https://sponsorhello.org/" />
+      <meta
+        name="og:site_name"
+        content="Sponsorhello - Fund the projects you love"
+      />
+      <meta name="og:type" content="website" />
     </>
   ),
 };
